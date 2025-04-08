19b8d22aba1e34e35337481b443de1844a1000b6
Dustin H <DustinHolder14@yahoo.com>
2025-04-08 16:19:56 -0500

    Fresh start, per course guidelines.

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,31 +1,34 @@
-const Hello = (props) => {
-  console.log(props)
-  return (
-  <div>
-    <p>Hello {props.name}, it looks like youre {props.age} years old.</p>
-  </div>
-  )
-}
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App = () => {
-  console.log("Hello from the <App /> Component")
-
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
-
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <Hello />
-      <Hello />
-      <Hello />
-      <p>Now HMR works</p>
-      <p>To get Hot Module Reloading to work, you need to use the WSL plugin in VSCode and open the WSL Distro via the command pallette.</p>
-      <p>Right now, the time is {now.toString()}</p>
-      <p>In other news: {a} plus {b} is {a+b}... Incredible stuff.</p>
-    </div>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
