ec2eec3e9a3148507255c2ddbd431df6a55adfce
Dustin H <dustinholder14@yahoo.com>
2025-04-08 17:50:03 -0500

    1.1 completed

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -8,6 +8,21 @@ const Header = (props) => {
 
 const Content = (props) => {
   // Add course part* and exercises* here
+  return (
+    <div>
+      <p>{props.parts[0]} - {props.exercises[0]} exercises</p>
+      <p>{props.parts[1]} - {props.exercises[1]} exercises</p>
+      <p>{props.parts[2]} - {props.exercises[2]} exercises</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Total exercises: {props.totalExercises}</p>
+    </div>
+  )
 }
 
 const App = () => {
@@ -19,10 +34,15 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
+  const allParts = [part1, part2, part3]
+  const allExercises = [exercises1, exercises2, exercises3]
+  const totalExercises = allExercises[0] + allExercises[1] + allExercises[2]
+
   return (
     <div>
       <Header course={course}/>
-      <Content />
+      <Content parts={allParts} exercises={allExercises}/>
+      <Total totalExercises={totalExercises} />
     </div>
   )
 }
