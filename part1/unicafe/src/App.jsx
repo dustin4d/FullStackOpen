e5947f7e4230a2637663cd009a4e0a2bc6de30bb
Dustin H <Dustinholder14@yahoo.com>
2025-04-23 15:24:22 -0500

    1.6 Complete -- revert back to starting template >_>

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,68 +1,14 @@
-import {useState} from 'react'
-
-const Button = (props) => {
-  return(
-    <div>
-      <button onClick={props.handleClick}>{props.text}</button>
-    </div>
-  )
-}
-
-const Feedback = (props) => {
-  return (
-    <div>
-      <h1>Give feedback</h1>
-      <Button text="good" handleClick={props.handleClick}></Button>
-      <Button text="neutral"></Button>
-      <Button text="bad"></Button>
-    </div>
-  )
-}
-
-const Total = (props) => {
-}
-
-
-const Stats = (props) => {
-    if (props.text === 'good') {
-        return (
-            <>
-            <p>Good reviews: {props.metric}</p>
-            </>
-        )
-    } else if (props.text === 'neutral') {
-        return (
-            <>
-            <p>Neutral reviews: {props.metric}</p>
-            </>
-        )
-    } else if (props.text === 'bad') {
-        return (
-            <>
-            <p>Bad reviews: {props.metric}</p>
-            </>
-        )
-    }
-}
+import { useState } from 'react'
 
 const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  const [totalGood, setTotalGood] = useState(0)
-  const [totalNeutral, setTotalNeutral] = useState(0)
-  const [totalBad, setTotalBad] = useState(0)
-
-  const handleClick = () => {
-    console.log("Click handled")
-  }
-
-  return(
+  return (
     <div>
-      <Feedback handleClick={handleClick}/>
-
-      <h1>Statistics</h1>
-      <Stats text="good" metric={totalGood}></Stats>
-      <Stats text="neutral" metric={totalNeutral}></Stats>
-      <Stats text="bad" metric={totalBad}></Stats>
+      code here
     </div>
   )
 }
