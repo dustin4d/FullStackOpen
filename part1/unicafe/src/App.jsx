669072f46fed6ea656b4ddbef933d1faf3b98f35
Dustin H <Dustinholder14@yahoo.com>
2025-04-28 16:32:40 -0500

    Unique event handler for each button.. i hate this

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,33 +6,50 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const handleClick = () => {
-    console.log("Clicked")
+  const handleGood = () => {
+    console.log("Good + 1")
+    setGood(good + 1)
+  }
+
+  const handleNeutral = () => {
+    console.log("Neutral + 1")
+    setNeutral(neutral + 1)
+  }
+
+  const handleBad = () => {
+    console.log("Bad + 1")
+    setBad(bad + 1)
   }
 
   return (
     <div>
-      <Feedback handleClick={handleClick}/>
+      <Feedback handleGood={handleGood} handleNeutral={handleNeutral} handleBad={handleBad}/>
+      <Stats metrics={[good, neutral, bad]}/>
     </div>
   )
 }
 
-const Feedback = ({text, handleClick}) => {
+const Feedback = (props) => {
     return (
         <div>
             <h1>Give feedback</h1>
-            <Button text='good' handleClick={handleClick}></Button>
-            <Button text='neutral'></Button>
-            <Button text='bad'></Button>
+            <button text='good' onClick={props.handleGood}>good</button>
+            <button text='neutral' onClick={props.handleNeutral}>neutral</button>
+            <button text='bad' onClick={props.handleBad}>bad</button>
             <hr></hr>
         </div>
     )
 }
 
-const Button = (props) => {
-    return (
-        <button onClick={props.handleClick}>{props.text}</button>
-    )
+const Stats = (props) => {
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>Good: {props.metrics[0]}</p>
+      <p>Neutral: {props.metrics[1]}</p>
+      <p>Bad: {props.metrics[2]}</p>
+    </div>
+  )
 }
 
 export default App
