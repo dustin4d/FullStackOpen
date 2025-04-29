561bf3f6ce847b4a6503a0f733dd33fc1f672c44
Dustin H <Dustinholder14@yahoo.com>
2025-04-29 13:37:44 -0500

    Averaging finally works holy shit

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,14 +5,22 @@ const App = () => {
     good: 0,
     neutral: 0,
     bad: 0,
+    total: 0,
+    avg: 0,
   })
 
   const handleGood = () => {
     setFeedback(prev => {
       const newFeedback = prev.good + 1
+      const newTotal = prev.total + 1
+      const score = (newFeedback * 1) + (prev.bad * -1)
+      const newAvg = score / newTotal
+
       const update = {
         ...prev,
         good: newFeedback,
+        total: newTotal,
+        avg: newAvg,
       }
 
       return update
@@ -22,9 +30,13 @@ const App = () => {
   const handleNeutral = () => {
     setFeedback(prev => {
       const newFeedback = prev.neutral + 1
+      const newTotal = prev.total + 1
+      // Neutral scores are not calculated in the average
+      
       const update = {
         ...prev,
         neutral: newFeedback,
+        total: newTotal,
       }
 
       return update
@@ -34,9 +46,15 @@ const App = () => {
   const handleBad = () => {
     setFeedback(prev => {
       const newFeedback = prev.bad + 1
+      const newTotal = prev.total + 1
+      const score = (prev.good * 1) + ((prev.bad + 1) * -1)
+      const newAvg = score / newTotal
+
       const update = {
         ...prev,
         bad: newFeedback,
+        total: newTotal,
+        avg: newAvg,
       }
 
       return update
@@ -71,8 +89,8 @@ const Stats = (props) => {
       <p>Neutral: {props.feedback.neutral}</p>
       <p>Bad: {props.feedback.bad}</p>
 
-      <p>Total: </p>
-      <p>Average: </p>
+      <p>Total: {props.feedback.total}</p>
+      <p>Average: {props.feedback.avg}</p>
     </div>
   )
 }
