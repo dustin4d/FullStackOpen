927c4ba1403ab66c06bd4ba48b9beb9a4e899522
Dustin H <Dustinholder14@yahoo.com>
2025-05-07 14:59:06 -0500

    2.4  complete -- handle arbitrary course amounts

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,73 +1,98 @@
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ] 
+
+  const getTotal = (course) => {
+    // Loop into each course object
+    return course.parts.reduce((acc, curr) => acc + curr.exercises, 0)
   }
 
-  const getTotal = () => {
-    const total = course.parts.reduce(
-      (sum, curr) => sum += curr.exercises, 0
-    )
-    return total
-  }
 
-  return <Course course={course} getTotal={getTotal}/>
+  // render each course object independently using Array.map 
+  return courses.map((course =>
+    <Course key={course.id} course={course} total={getTotal(course)}/>
+  ))
+
 }
 
 const Course = (props) => {
-return (
-  <div>
-    <Header course={props.course}/>
-    <Content course={props.course}/>
-    <Total getTotal={props.getTotal} />
-  </div>
-)
+  return (
+    <div>
+      <Header course={props.course}/>
+      <Content course={props.course}/>
+      <Total total={props.total} />
+    </div>
+  )
 }
 
 const Header = (props) => {
-return (
-  <h1>{props.course.name}</h1>
-)
+  return (
+    <h1>{props.course.name}</h1>
+  )
 }
 
 const Content = (props) => {
-return(
-  <div>
-    {props.course.parts.map(part => <Part key={part.id} name={part.name} exercise={part.exercises}/>)}
-  </div>
-)
+  // `course` is an array, so use map to go through it
+  return(
+    <div>
+      <Part part={props.course.parts} />
+    </div>
+  )
 }
 
 const Part = (props) => {
-return (
-  <p>{props.name} {props.exercise}</p>
-)
+  return props.part.map(part => <li key={part.id}>{part.name} - {part.exercises}</li>)
 }
 
 const Total = (props) => {
-
-return (
-  <div>
-    <p>Total number of exercises: {props.getTotal()}</p>
-  </div>
-)
+  console.log(props)
+  return (
+    <div>
+      <p>Total number of exercises: {props.total}</p>
+    </div>
+  )
 } 
 
 export default App
